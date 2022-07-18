084458379813d64c2db1cd28f0a081223ae3c2d0
A <97649928+the-decentralizer@users.noreply.github.com>
2022-07-18 13:08:26 -0400

    Initialized repo while at SVG steps

---
+++ scripts/run.js
@@ -0,0 +1,25 @@
+const main = async () => {
+  const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to:", nftContract.address);
+
+  //call mint
+  let txn = await nftContract.makeAnEpicNFT();
+  await txn.wait();
+  //make another for gigs
+  txn = await nftContract.makeAnEpicNFT();
+  await txn.wait();
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
